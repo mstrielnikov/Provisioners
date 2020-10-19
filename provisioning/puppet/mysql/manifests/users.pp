098f0681910c23aeed1274c1a1ccd9447bb3be88
mstrielnikov <makstr@ukr.net>
2020-10-19 20:11:52 +0300

    puppet mysql module

--- provisioning/puppet/mysql/manifests/users.pp
+++
@@ -1,9 +0,0 @@
-mysql::db { 'mydb':
-  user     => 'myuser',
-  password => 'mypass',
-  host     => 'localhost',
-  grant    => ['ALL PRIVILEGES'],
-  sql      => '/home/backup/mysql/mydb/backup.gz',
-  import_cat_cmd => 'zcat',
-  import_timeout => 900
-}
